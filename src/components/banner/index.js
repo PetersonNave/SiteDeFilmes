e13e98fadce7989d9be2bf34db5a0209eeaf62e5
PetersonNAve <petersonmelo.nave@gmail.com>
2022-02-06 03:17:18 -0300

    Adição de botões de navegação e seus respectivos funcionamentos; Inclusão de alguns detalhes estéticos

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -5,13 +5,22 @@ export default(item)=>{
 
     const [movieChanged, SetMovieChanged] = useState(null)
     const [voteAverage, setVoteAverage] = useState(0);
+    const [overviewText, setOverviewText] = useState(null)
+
+
     useEffect(()=>{
 
         const banner = async () =>{
             const random = Math.floor(Math.random() * 20);       
             SetMovieChanged(item.item[0].items.results[random])
-            setVoteAverage(item.item[0].items.results[random].vote_average);   
-            
+            setVoteAverage(item.item[0].items.results[random].vote_average); 
+
+            if((item.item[0].items.results[random].overview).slice(-1) == '.'){
+                setOverviewText((item.item[0].items.results[random].overview).slice(0, 350) + "...")
+            }else{
+                setOverviewText((item.item[0].items.results[random].overview).slice(0, 350))
+            }
+           
         }       
     banner();
    
@@ -33,7 +42,7 @@ return(
                 }
             
             <div className='airYear'>{(movieChanged.first_air_date).slice(0,4)}</div>
-            <div className='bannerDescription'>{movieChanged.overview}</div>
+            <div className='bannerDescription'>{overviewText}</div>
             <div className='info'>
                 
                 <div className='points'>{voteAverage * 10}% relevante</div>
--- src/components/bigList/index.js
+++ src/components/bigList/index.js
@@ -1,34 +1,64 @@
 import {React, useState} from 'react';
 import './style.css'
 export default(item)=>{
-
-
-    
+ 
     const itemUn = item.item;
-    const [section, setSession] = useState(itemUn.items.results);
-   
+    const [section, setSession] = useState(itemUn.items.results); 
+    const [xPosition, setXPosition] = useState(30);
+
+    const clickLeft =() =>{
+        if(xPosition - Math.round(window.innerWidth)< -4720){
+           setXPosition(-5030+Math.round(window.innerWidth));
+        }else{
+            setXPosition(xPosition-Math.round(window.innerWidth/2));
+        }
+        console.log(xPosition - Math.round(window.innerWidth));
+    }
+    const clickRight = () =>{
+        if(xPosition+Math.round(window.innerWidth/2) >= 30){
+            setXPosition(30);
+         }else{
+             setXPosition(xPosition+Math.round(window.innerWidth/2));
+         }
+         console.log(xPosition - Math.round(window.innerWidth));
+    }
+
 return(
 
 <div>
-
-
-<section>
-    <h2>{itemUn.title}</h2>
-    <div className='movieRowListArea'>
-        <div className='movieRowList'>       
-    {section.map ((item, key)=>(
-        <div key={key} className='movieRow'>
-            <a href='https://s20.fofoquei.com/storage1/tuegaymano/videos/series/sobrenatural/1x1.mp4'>
-            <img className='movie' key={key} src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}/>
-            </a>
-          
+    <section>
+        <h2 className='rowName'>{itemUn.title}</h2>
+        <div className='movieRowListArea'>
+           
+            <div className='movieRowList' style={{
+            
+              width: section.length * 250,  
+              marginLeft:  xPosition
+              
+            }}>    
+            <div className='arrowRight' onClick={clickLeft}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16">
+                    <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/>
+                </svg>
+            </div>
+            {xPosition !== 30 &&
+             <div className='arrowLeft' onClick={clickRight}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left" viewBox="0 0 16 16">
+                    <path d="M10 12.796V3.204L4.519 8 10 12.796zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z"/>
+                </svg>
+             </div>   
+            }
+           
+                {section.map ((item, key)=>(
+                    <div key={key} className='movieRow'>
+                        <div >
+                            <img className='movie' key={key} src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}/>
+                        </div>       
+                    </div>
+            ))}
+            </div>
         </div>
-         
-         ))}
-         </div>
-    </div>
-</section>
-
+    </section>
 </div>
 
 
